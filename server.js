9a6fee2e73c301b310b56336ee87c0064423ae08
mopox2233-a11y <mopox2233@gmail.com>
2026-08-09 05:17:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,94 @@
+<div class="price">
+استفسر عن السعر
+</div>
 
-import express from "express";
+<a
+class="productBtn"
+target="_blank"
+href="${WHATSAPP}?text=السلام%20عليكم%20أريد%20الاستفسار%20عن%20AirPods"
+>
+💬 اطلب عبر واتساب
+</a>
 
-const app = express();
+</div>
 
-app.get("/", (req, res) => {
-  res.send("MOHAMMED MOBILE STORE WORKS ✅");
-});
+</div>
 
-export default app;
+
+<div class="product">
+
+<div class="productImage">
+🔋
+</div>
+
+<div class="productInfo">
+
+<h3>
+Power Bank
+</h3>
+
+<div class="price">
+استفسر عن السعر
+</div>
+
+<a
+class="productBtn"
+target="_blank"
+href="${WHATSAPP}?text=السلام%20عليكم%20أريد%20الاستفسار%20عن%20Power%20Bank"
+>
+💬 اطلب عبر واتساب
+</a>
+
+</div>
+
+</div>
+
+
+<div class="product">
+
+<div class="productImage">
+🔌
+</div>
+
+<div class="productInfo">
+
+<h3>
+الشواحن
+</h3>
+
+<div class="price">
+استفسر عن السعر
+</div>
+
+<a
+class="productBtn"
+target="_blank"
+href="${WHATSAPP}?text=السلام%20عليكم%20أريد%20الاستفسار%20عن%20الشواحن"
+>
+💬 اطلب عبر واتساب
+</a>
+
+</div>
+
+</div>
+
+
+</div>
+
+</div>
+
+</section>
+
+
+<section id="offers">
+
+<div class="container">
+
+<div class="offer">
+
+<h2>
+🔥 عروض MOHAMMED MOBILE STORE
+</h2>
+
+<p class="muted">
+للاستفسار عن آخر العروض والأسعار، تواصل ويانا مباشرة
